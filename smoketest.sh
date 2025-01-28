f57b5ecee91cee28663b5d77c62bb07355fd2457
Anastasios Papalyras <tasos@rdnt.dev>
2025-01-28 13:17:12 +0200

    Add sharpmem (#724)
    sharpmem: add implementation of sharpmem display driver
    
    * Implement Configure, Clear and ClearBuffer, add some tests, add documentation/comments
    * Reverse white
    * Inverted bits, fix and improve ClearBuffer, cleanup
    * Refine doc comment
    * Driver refactor, optimizations toggle, additional tests & support for all SKUs
    * Fix address overflow padding, add wire-level tests for assumed address encoding
    * Minor rename
    * Cleanup and doc fixes
    * Add device configs
    * Bounds check
    * Add example and smoketest entry
    * Use uf2 output file format
    * Refine example

--- smoketest.sh
+++ smoketest.sh
@@ -137,6 +137,7 @@ tinygo build -size short -o ./build/test.hex -target=macropad-rp2040 ./examples/
 tinygo build -size short -o ./build/test.hex -target=macropad-rp2040 ./examples/encoders/quadrature-interrupt
 tinygo build -size short -o ./build/test.uf2 -target=pico ./examples/mcp9808/main.go
 tinygo build -size short -o ./build/test.hex -target=pico ./examples/tmc5160/main.go
+tinygo build -size short -o ./build/test.uf2 -target=nicenano ./examples/sharpmem/main.go
 # network examples (espat)
 tinygo build -size short -o ./build/test.hex -target=challenger-rp2040 ./examples/net/ntpclient/
 # network examples (wifinina)
